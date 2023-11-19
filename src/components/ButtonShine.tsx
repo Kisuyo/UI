8b3366c03d0d91be39e69240dc78a7e1afd41475
Kisuyo <ig.intr3st@gmail.com>
2023-11-19 16:34:29 +0100

    .

---
+++ src/components/ButtonShine.tsx
@@ -0,0 +1,34 @@
+export default function ButtonShine() {
+  return (
+    <>
+      <style>
+        {`
+        #ButtonShine {
+          background-image:
+            linear-gradient(
+              124deg in hsl,
+              hsl(0 0% 20%) 35% 35%, hsl(253 0% 31%) 50%, hsl(0 0% 20%) 65% 65%
+            );
+          background-size: 200% 100%;
+          background-position: 100% 0%;
+
+        }
+        #ButtonShine:hover {
+          background-position: 0% 0%;
+          background-repeat: no-repeat;
+        }
+        `}
+      </style>
+      <div class="w-[200px] h-[60px]">
+        <div
+          class="rounded-full w-full transition-all h-full p-0.5 text-white"
+          id="ButtonShine"
+        >
+          <div class="px-8 p-2 bg-neutral-900 rounded-full font-light transition-all">
+            Click me!
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { Match, Show, Switch, createSignal } from "solid-js"
 import { useGlobal } from "~/Global/global"
+import ButtonShine from "~/components/ButtonShine"
 
 export default function Home() {
   const global = useGlobal()
   return (
     <>
-      <div></div>
+      <div class="bg-neutral-900 w-screen h-screen flex items-center justify-center">
+        <ButtonShine />
+      </div>
     </>
   )
 }
